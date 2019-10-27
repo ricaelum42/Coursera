41b3f1cc3ba9f80fae9fe63683fdfac2706038e8
球形闪电 <fredericaelum@hotmail.com>
2019-10-27 16:38:36 -0400

    Create join2_mapper.py

---
+++ Hadoop Platform and Application Framework/Week 4/join2_mapper.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import sys
+
+for line in sys.stdin:
+    line = line.strip()
+    key_value = line.split(",")
+    
+    if key_value[1][1:].isdigit():
+        print("%s\t%s".format(key_value[0], key_value[1][1:]))
+    else:
+        if key_value[1][1:] == "ABC":
+            print("%s\t%s".format(key_value[0], key_value[1][1:]))

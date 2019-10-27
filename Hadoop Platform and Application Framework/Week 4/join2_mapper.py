c000eef9744b6f49179fad775900a408f3bb107e
球形闪电 <fredericaelum@hotmail.com>
2019-10-27 16:42:26 -0400

    Update join2_mapper.py

--- Hadoop Platform and Application Framework/Week 4/join2_mapper.py
+++ Hadoop Platform and Application Framework/Week 4/join2_mapper.py
@@ -7,7 +7,7 @@ for line in sys.stdin:
     key_value = line.split(",")
     
     if key_value[1][1:].isdigit():
-        print("%s\t%s".format(key_value[0], key_value[1][1:]))
+        print("%s\t%s" % (key_value[0], key_value[1][1:]))
     else:
         if key_value[1][1:] == "ABC":
-            print("%s\t%s".format(key_value[0], key_value[1][1:]))
+            print("%s\t%s" % (key_value[0], key_value[1][1:]))
